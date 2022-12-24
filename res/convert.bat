a7d57b6e0f91620d7cf85300a5bf0414bce34be2
zlago <104219492+zlago@users.noreply.github.com>
2022-12-24 13:50:17 +0100

    add graphics i guess

---
+++ res/convert.bat
@@ -0,0 +1,4 @@
+@ ECHO OFF
+rgbgfx -v -d 2 -u --colors "#420, #fff, #000, #f00" -o ..\src\res\screen.2bpp -t ..\src\res\screen.tilemap png\screen.png
+rgbgfx -v -d 2 --colors "#420, #fff, #000, #f00" -o ..\src\res\font.2bpp png\font.png
+pause
--- src/main.asm
+++ src/main.asm
@@ -13,5 +13,25 @@ SECTION "header", ROM0[$100]
 
 SECTION "init", ROM0
 Init: ; init code should go here
-	
+	; load tilemap
+	ld hl, xScreen2bpp
+	ld de, _VRAM
+	ld bc, xScreen2bpp.end - xScreen2bpp
+	call SafeCpy
+	; load tiles
+	ld hl, xScreenTilemap
+	ld de, _SCRN0
+	ld bc, xScreenTilemap.end - xScreenTilemap
+	call SafeCpy
 	:jr:- ; endless loop
+
+SECTION "data", ROMX
+xScreen2bpp:
+	incbin "screen.2bpp"
+	.end
+xScreenTilemap:
+	incbin "screen.tilemap"
+	.end
+xFont:
+	incbin "font.2bpp"
+	.end
