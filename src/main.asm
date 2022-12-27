3418d920e8f62b9a22712484425a31a307d03828
zlago <104219492+zlago@users.noreply.github.com>
2022-12-27 10:49:23 +0100

    properly init VRAM
    init all of VRAM, use VRAM sections for saner VRAM management

--- src/main.asm
+++ src/main.asm
@@ -13,16 +13,23 @@ SECTION "header", ROM0[$100]
 
 SECTION "init", ROM0
 Init: ; init code should go here
-	; load tilemap
-	ld hl, xScreen2bpp
-	ld de, _VRAM
-	ld bc, xScreen2bpp.end - xScreen2bpp
-	call SafeCpy
-	; load tiles
-	ld hl, xScreenTilemap
-	ld de, _SCRN0
-	ld bc, xScreenTilemap.end - xScreenTilemap
-	call SafeCpy
+	; init graphics
+		; load tilemap
+		ld hl, xScreen2bpp
+		ld de, vScreen
+		ld bc, xScreen2bpp.end - xScreen2bpp
+		call SafeCpy
+		; load font
+		ld hl, xFont2bpp
+		ASSERT fail, vScreen + (xScreen2bpp.end - xScreen2bpp) == vFont, "VRAM assertion failed!"
+		ld bc, xFont2bpp.end - xFont2bpp
+		call SafeCpy
+		; load tiles
+		ld hl, xScreenTilemap
+		ld de, _SCRN0
+		ld bc, xScreenTilemap.end - xScreenTilemap
+		call SafeCpy
+	; 
 	:jr:- ; endless loop
 
 SECTION "data", ROMX
@@ -32,6 +39,14 @@ xScreen2bpp:
 xScreenTilemap:
 	incbin "screen.tilemap"
 	.end
-xFont:
+xFont2bpp:
 	incbin "font.2bpp"
+	.end
+
+SECTION "vram", VRAM
+vScreen:
+	ds xScreen2bpp.end - xScreen2bpp
+	.end
+vFont:
+	ds xFont2bpp.end - xFont2bpp
 	.end
