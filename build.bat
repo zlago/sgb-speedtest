42ec1429258c218d864347bc4cd88b7891535385
zlago <104219492+zlago@users.noreply.github.com>
2022-12-27 17:34:55 +0100

    change binary name to be consistent with repo name

--- build.bat
+++ build.bat
@@ -4,14 +4,14 @@
 rgbasm -h -Wall -i src/inc/ -i src/res -o obj\main.o         src\main.asm
 rgbasm -h -Wall -i src/inc/ -i src/res -o obj\length.o       src\length.asm
 rgbasm -h -Wall -i src/inc/ -i src/res -o obj\delayplotter.o src\delayplotter.asm
-rgbasm -h -Wall -i src/inc/ -i src/res -o obj\ramplotter.o src\ramplotter.asm
+rgbasm -h -Wall -i src/inc/ -i src/res -o obj\ramplotter.o   src\ramplotter.asm
 rgbasm -h -Wall -i src/inc/ -i src/res -o obj\init.o         src\init.asm
 rgbasm -h -Wall -i src/inc/ -i src/res -o obj\sub.o          src\sub.asm
 :: link the files, spit out a rom, a sym, and a map
-rgblink -p 0xFF -m bin\packettest.map -n bin\packettest.sym -o bin\packettest.gb obj\main.o obj\length.o obj\delayplotter.o obj\ramplotter.o obj\init.o obj\sub.o
+rgblink -p 0xFF -m bin\sgb-speedtest.map -n bin\sgb-speedtest.sym -o bin\sgb-speedtest.gb obj\main.o obj\length.o obj\delayplotter.o obj\ramplotter.o obj\init.o obj\sub.o
 :: fix the header
-rgbfix -l 0x33 -j -s -v -p 0xFF -t "PACKET TEST" bin/packettest.gb
+rgbfix -l 0x33 -j -s -v -p 0xFF -t "SPEED TEST" bin/sgb-speedtest.gb
 :: check the rom ussage (optional, i like how it looks)
-romusage bin\packettest.map -g
+romusage bin\sgb-speedtest.map -g
 :: done! pause to let the user see any errors
 pause
